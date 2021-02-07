c82edd71fa46843919d1f319d5365c128a68c315
binodswain <binodswain@users.noreply.github.com>
2021-02-07 18:51:38 +0000

    deployed via Gatsby Action (be5259e69249117fda5efe5b09a736ddc840c71b)

---
+++ docs/252f366e-ad99277698b527890246.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{NIcq:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-ad99277698b527890246.js.map
